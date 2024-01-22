9afb25ed2d6c160b6dc1e2dd460f6972226531e5
Nick Koole <nickkoole@hotmail.com>
2024-01-22 15:32:40 +0100

    Verrukkulluk controller aangemaakt

---
+++ Backend/Verrukkulluk/Controllers/VerrukkullukController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Verrukkulluk.Models;
+
+namespace Verrukkulluk.Controllers
+{
+    public class VerrukkullukController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
