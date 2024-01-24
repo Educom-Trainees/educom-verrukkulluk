b7e8718e3b1ef16c177b95e946e1e0ef0b4b6f8e
Tim-Vaes <timvaes1995@gmail.com>
2024-01-24 15:08:24 +0100

    Logica events werkt met losse data zonder gebruik database

--- Backend/Verrukkulluk/Controllers/VerrukkullukController.cs
+++ Backend/Verrukkulluk/Controllers/VerrukkullukController.cs
@@ -14,9 +14,59 @@ namespace Verrukkulluk.Controllers
         {
             return View("MyRecipes");
         }
-        public IActionResult Event()
+
+        public IActionResult Event(string eventName)
         {
-            return View("Event");
+            Event eventModel = GetEventData(eventName);
+            return View(eventModel);
         }
+
+        private Event GetEventData(string eventName)
+        {
+            switch (eventName?.ToLower())
+            {
+                default:
+                    return new Event
+                    {
+                        Title = "Vegetarisch koken",
+                        Description = "Een workshop vegetarisch koken, onder leiding van Trientje Hupsakee",
+                        Date = new DateOnly(2024, 01, 30),
+                        StartTime = new TimeOnly(14, 0),
+                        EndTime = new TimeOnly(16, 0),
+                        Place = "Jaarbeurs Utrecht"
+                    };
+                case "test1":
+                    return new Event
+                    {
+                        Title = "Test1",
+                        Description = "Test1 description",
+                        Date = new DateOnly(2024, 01, 30),
+                        StartTime = new TimeOnly(14, 0),
+                        EndTime = new TimeOnly(16, 0),
+                        Place = "Jaarbeurs Utrecht"
+                    };
+                case "test2":
+                    return new Event
+                    {
+                        Title = "Test2",
+                        Description = "Test2 description",
+                        Date = new DateOnly(2024, 01, 30),
+                        StartTime = new TimeOnly(14, 0),
+                        EndTime = new TimeOnly(16, 0),
+                        Place = "Jaarbeurs Utrecht"
+                    };
+                case "test3":
+                    return new Event
+                    {
+                        Title = "Test3",
+                        Description = "Test3 description",
+                        Date = new DateOnly(2024, 01, 30),
+                        StartTime = new TimeOnly(14, 0),
+                        EndTime = new TimeOnly(16, 0),
+                        Place = "Jaarbeurs Utrecht"
+                    };
+            }
+        }
+
     }
 }
