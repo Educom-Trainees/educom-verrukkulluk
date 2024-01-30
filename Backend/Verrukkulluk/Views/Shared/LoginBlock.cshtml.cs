f3ca906450c927be1972e9fb4e09761b438a4c0b
Rogier Be <rogier.m.be@gmail.com>
2024-01-30 12:03:03 +0100

    #49 beginnetje nieuwe file

---
+++ Backend/Verrukkulluk/Views/Shared/LoginBlock.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Verrukkulluk.Views.Shared
+{
+    public class LoginBlockModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
