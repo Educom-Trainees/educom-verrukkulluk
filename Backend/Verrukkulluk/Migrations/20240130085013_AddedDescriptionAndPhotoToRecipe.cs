09fc71603ff1894569c2699a42ef010f46b2009c
Nicole Goris <nicolegoris@hotmail.nl>
2024-01-30 09:52:04 +0100

    Added migration for added recipe properties

---
+++ Backend/Verrukkulluk/Migrations/20240130085013_AddedDescriptionAndPhotoToRecipe.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Verrukkulluk.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedDescriptionAndPhotoToRecipe : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Recipes",
+                type: "varchar(500)",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "DishPhoto",
+                table: "Recipes",
+                type: "longblob",
+                nullable: false);
+
+            migrationBuilder.CreateTable(
+                name: "Allergy",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    AllergyId = table.Column<int>(type: "int", nullable: true),
+                    RecipeId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Allergy", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Allergy_Allergy_AllergyId",
+                        column: x => x.AllergyId,
+                        principalTable: "Allergy",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Allergy_Recipes_RecipeId",
+                        column: x => x.RecipeId,
+                        principalTable: "Recipes",
+                        principalColumn: "Id");
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Allergy_AllergyId",
+                table: "Allergy",
+                column: "AllergyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Allergy_RecipeId",
+                table: "Allergy",
+                column: "RecipeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Allergy");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Recipes");
+
+            migrationBuilder.DropColumn(
+                name: "DishPhoto",
+                table: "Recipes");
+        }
+    }
+}
