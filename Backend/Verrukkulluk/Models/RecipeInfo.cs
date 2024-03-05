fa2a2303ba5eee7dad92ad9387214b042f5691eb
Thomas-van-Haastrecht <thomas.hjvh@gmail.com>
2024-03-05 09:12:18 -0500

    Removed unused constructor

--- Backend/Verrukkulluk/Models/RecipeInfo.cs
+++ Backend/Verrukkulluk/Models/RecipeInfo.cs
@@ -11,37 +11,6 @@ namespace Verrukkulluk.Models
         public ICollection<Allergy> Allergies { get; set; } = new List<Allergy>();
 
         public RecipeInfo() { }
-        public RecipeInfo(string title, KitchenType kitchenType, string description, string[] instructions, double rating, User creator, int imageObjId, List<Ingredient> ingredients, int numberOfPeople) :
-                     base(title, kitchenType, description, instructions, rating, creator, imageObjId, ingredients, numberOfPeople)
-        {
-            Price = ingredients.Select(i => i.Product.Price * (decimal)Math.Ceiling(i.Amount / i.Product.Amount)).Sum().ToString("F2");
-            Calories = (int)ingredients.Select(i => i.Product.Calories * i.Amount / i.Product.Amount).Sum();
-            Allergies = Ingredients.Select(i => i.Product).SelectMany(p => p.ProductAllergies).Select(p => p.Allergy).Distinct().ToList();
-            if (Allergies.Where(a => a.Name == "Vlees").Any())
-            {
-                DishTypes.Add(new DishType("Vlees"));
-            }
-            if (Allergies.Where(a => a.Name == "Vis").Any())
-            {
-                DishTypes.Add(new DishType("Vis"));
-            }
-            if (!(Allergies.Where(a => a.Name == "Vlees").Any() | Allergies.Where(a => a.Name == "Vis").Any() | Allergies.Where(a => a.Name == "Schaald").Any() | Allergies.Where(a => a.Name == "Weekdieren").Any()))
-            {
-                DishTypes.Add(new DishType("Vegetarisch"));
-                if (!(Allergies.Where(a => a.Name == "Ei").Any() | Allergies.Where(a => a.Name == "Lactose").Any()))
-                {
-                    DishTypes.Add(new DishType("Vegan"));
-                }
-            }
-            if (!Allergies.Where(a => a.Name == "Lactose").Any())
-            {
-                DishTypes.Add(new DishType("Lactosevrij"));
-            }
-            if (!Allergies.Where(a => a.Name == "Gluten").Any())
-            {
-                DishTypes.Add(new DishType("Glutenvrij"));
-            }
-        }
 
         public RecipeInfo(Recipe recipe) : base(recipe.Title, recipe.KitchenType, recipe.Description, recipe.Instructions, recipe.AverageRating, recipe.Creator, recipe.ImageObjId, recipe.Ingredients.ToList(), recipe.NumberOfPeople)
         {
