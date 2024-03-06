1e776b7fb16cc24ca4c64f5eb3381f152c07df0f
LauraAnneB <88866166+LauraAnneB@users.noreply.github.com>
2024-03-06 14:15:31 +0100

    Created new 'EventController' that handles events and event participants.

--- Backend/Verrukkulluk/Controllers/VerrukkullukController.cs
+++ Backend/Verrukkulluk/Controllers/VerrukkullukController.cs
@@ -156,43 +156,7 @@ namespace Verrukkulluk.Controllers
             }
         }
 
-        public IActionResult Event(int id)
-        {
-            EventModel.Event = Servicer.GetEventById(id);
-            return View(EventModel);
-        }
-
-        public IActionResult JoinEvent(int id)
-        {
-            EventModel.Event = Servicer.GetEventById(id);
-
-            return View("EventParticipation", EventModel);
-        }
-
-
-        [HttpPost]
-        public IActionResult EventSignUp(string name, string email, int EventId)
-        {
-            if (!string.IsNullOrEmpty(name) && !string.IsNullOrEmpty(email))
-            {
-                
-                EventModel.Event = Servicer.AddParticipantToEvent(name, email, EventId);
-
-                ViewBag.Name = name;
-                ViewBag.Email = email;
-                ViewBag.EventId = EventId;
-
-                return View("ThankYou", EventModel);
-            }
-            else
-            {
-                ModelState.AddModelError(string.Empty, "Please provide valid name and email.");
-                return View("EventParticipation");
-            }
-        }
-
-
-
+        //events 
 
 
             [HttpPost]
