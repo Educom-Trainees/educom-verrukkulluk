7fbe2b7d68afd4f4ebe80f233bc01e76e12facff
JeroenHeemskerk <j.w.heemskerk@gmail.com>
2024-03-14 13:53:32 +0100

    Removed Id as Primary Key for ProductAllergy, made combined PK. Also removed link allergy to recipe

---
+++ Backend/Verrukkulluk/Migrations/20240314123534_CombinePk.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Verrukkulluk.Migrations
+{
+    /// <inheritdoc />
+    public partial class CombinePk : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Allergies_Recipes_RecipeId",
+                table: "Allergies");
+            
+            migrationBuilder.DropIndex(
+                name: "IX_Allergies_RecipeId",
+                table: "Allergies");
+            
+            migrationBuilder.DropColumn(
+                name: "RecipeId",
+                table: "Allergies");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductAllergies_Recipes_RecipeId",
+                table: "ProductAllergies");
+            
+            migrationBuilder.DropIndex(
+                name: "IX_ProductAllergies_RecipeId",
+                table: "ProductAllergies");
+
+            migrationBuilder.DropColumn(
+                name: "RecipeId",
+                table: "ProductAllergies");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ProductAllergies",
+                table: "ProductAllergies");
+           
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "ProductAllergies");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ProductAllergies",
+                table: "ProductAllergies",
+                columns: new[] { "ProductId", "AllergyId" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ProductAllergies",
+                table: "ProductAllergies");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "ProductAllergies",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+
+            // Make sure the id is unique
+            migrationBuilder.Sql("UPDATE ProductAllergies SET Id=ProductId * 100 + AllergyId");
+
+            migrationBuilder.AddColumn<int>(
+                name: "RecipeId",
+                table: "ProductAllergies",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ProductAllergies",
+                table: "ProductAllergies",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductAllergies_RecipeId",
+                table: "ProductAllergies",
+                column: "RecipeId");
+
+            migrationBuilder.AddColumn<int>(
+                name: "RecipeId",
+                table: "Allergies",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Allergies_RecipeId",
+                table: "Allergies",
+                column: "RecipeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Allergies_Recipes_RecipeId",
+                table: "Allergies",
+                column: "RecipeId",
+                principalTable: "Recipes",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductAllergies_Recipes_RecipeId",
+                table: "ProductAllergies",
+                column: "RecipeId",
+                principalTable: "Recipes",
+                principalColumn: "Id");
+        }
+    }
+}
--- Backend/Verrukkulluk/Models/DbModels/ProductAllergy.cs
+++ Backend/Verrukkulluk/Models/DbModels/ProductAllergy.cs
@@ -1,11 +1,15 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace Verrukkulluk
 {
     public class ProductAllergy
     {
-        public int Id { get; set; }
+        public int AllergyId { get; set; }
+        [ForeignKey(nameof(AllergyId))]
         public Allergy Allergy { get; set; }
+        public int ProductId { get; set; }
+        [ForeignKey(nameof(ProductId))]
         [JsonIgnore]
         public Product Product { get; set; }
         public ProductAllergy() { }
