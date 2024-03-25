c0d46e99c0439d598cf260bf452fe38f6280af96
JeroenHeemskerk <j.w.heemskerk@gmail.com>
2024-03-25 09:46:35 +0100

    Fixed that FavouriteRecipies is a many-to-many

---
+++ Backend/Verrukkulluk/Migrations/20240322173841_RemovedUserFromIngredients.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Verrukkulluk.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovedUserFromIngredients : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Ingredients_AspNetUsers_UserId",
+                table: "Ingredients");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Ingredients_UserId",
+                table: "Ingredients");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Ingredients");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Ingredients",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ingredients_UserId",
+                table: "Ingredients",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Ingredients_AspNetUsers_UserId",
+                table: "Ingredients",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
---
+++ Backend/Verrukkulluk/Migrations/20240325083936_FavoriteRecipes.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Verrukkulluk.Migrations
+{
+    /// <inheritdoc />
+    public partial class FavoriteRecipes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FavoriteRecipes",
+                columns: table => new
+                {
+                    FavouritesListId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FavoriteRecipes", x => new { x.FavouritesListId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_FavoriteRecipes_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_FavoriteRecipes_Recipes_FavouritesListId",
+                        column: x => x.FavouritesListId,
+                        principalTable: "Recipes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FavoriteRecipes_UserId",
+                table: "FavoriteRecipes",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FavoriteRecipes");
+        }
+    }
+}
