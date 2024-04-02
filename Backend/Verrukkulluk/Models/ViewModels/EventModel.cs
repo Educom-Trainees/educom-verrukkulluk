5438596b078b0fdaff3b10b30dd572543eb4c6d6
LauraAnneB <88866166+LauraAnneB@users.noreply.github.com>
2024-04-02 16:06:25 +0200

    removed some code

--- Backend/Verrukkulluk/Models/ViewModels/EventModel.cs
+++ Backend/Verrukkulluk/Models/ViewModels/EventModel.cs
@@ -5,23 +5,13 @@ namespace Verrukkulluk.Models
 {
     public class EventModel:VerModel, IEventModel
     {
-        private readonly UserManager<User> _userManager;
-        private readonly IHttpContextAccessor _httpContextAccessor;
 
         public Event Event { get; set; }
 
         public EventModel() { }
         public EventModel(ICrud crud, UserManager<User> userManager, IHttpContextAccessor httpContextAccessor, SignInManager<User> signInManager) : base(crud, userManager, httpContextAccessor, signInManager)
         {
-            _userManager = userManager;
-            _httpContextAccessor = httpContextAccessor;
-        }
 
-
-        public async Task<string> GetCurrentUserEmail()
-        {
-            var user = await _userManager.GetUserAsync(_httpContextAccessor.HttpContext.User);
-            return user.Email;
         }
 
     }
